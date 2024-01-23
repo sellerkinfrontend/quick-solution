088626c4c19f253cb5f04bfe75d46e014f529ca8
manishrohila <manishrohila0507@gmail.com>
2024-01-23 22:48:22 +0530

    fix: question bold in faq section

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -23,47 +23,47 @@ const Faq = () => {
             </div>
             <div className='mb-10 font-serif'>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'>Would I be able to connect my domain with the shopify website ?</p>
+                    <p className='mb-2 font-medium'>Would I be able to connect my domain with the shopify website ?</p>
                     <p>Yes, Absolutely, you would be able to connect your domain with the shopify website </p>
                 </div>
 
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'> What are the benefits of using Shopify ?</p>
+                    <p className='mb-2 font-medium'> What are the benefits of using Shopify ?</p>
                     <p>Easy to use - Shopify is very user friendly  </p>
                     <p className='mt-2'>Great Designs - Shopify offers many professional and modern themes that you can use for your online store </p>
                     <p className='mt-2'>Affordable - Shopify is one of the most affordable ecommerce platforms on the market. With plans starting at just $29 per month plus you don’t need to worry about additional bandwidth or hosting costs since all of that is included in your monthly plan. </p>
                     <p className='mt-2'>Excellent Support - Shopify offers excellent support and helps resources if you ever have any questions or need assistance with your store.</p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'>Do you make the Shopify Store Mobile Friendly ?</p>
+                    <p className='mb-2 font-medium'>Do you make the Shopify Store Mobile Friendly ?</p>
                     <p>Yes, all the shopify stores we develop are mobile-friendly by default</p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'> Can you integrate Third Party apps and tools into my shopify store ?</p>
+                    <p className='mb-2 font-medium'> Can you integrate Third Party apps and tools into my shopify store ?</p>
                     <p>Yes, we can integrate third party apps and tools in your shopify store</p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'>Do I need any technical skills to manage my shopify website ?</p>
+                    <p className='mb-2 font-medium'>Do I need any technical skills to manage my shopify website ?</p>
                     <p>No, You don’t need any technical skills to manage your shopify website </p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'> How will I keep a check on the progress of my shopify website development project ?</p>
+                    <p className='mb-2 font-medium'> How will I keep a check on the progress of my shopify website development project ?</p>
                     <p>The dedicated developer will be assigned to your shopify store development will keep you informed about the progress through emails and chats  </p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'>How long does it take to complete the shopify standard website ?</p>
+                    <p className='mb-2 font-medium'>How long does it take to complete the shopify standard website ?</p>
                     <p>We can deliver the Shopify standard website in 7 -21 days </p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'> Can I reach out to you for minor changes for my shopify store ?</p>
+                    <p className='mb-2 font-medium'> Can I reach out to you for minor changes for my shopify store ?</p>
                     <p>Yes, Absolutely, you can reach out to us for minor changes</p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'> How do I know if Shopify is the right platform for my business ?</p>
+                    <p className='mb-2 font-medium'> How do I know if Shopify is the right platform for my business ?</p>
                     <p>Shopify is the right platform for ecommerce businesses, Moreover, it provides you the ecosystem.</p>
                 </div>
                 <div className='flex flex-col  w-10/12 md:w-7/12 mx-auto p-4  rounded-xl bg-custom-color-2 shadow-lg mt-10'>
-                    <p className='mb-2'> Do you offer post development Services ?</p>
+                    <p className='mb-2 font-medium'> Do you offer post development Services ?</p>
                     <p>Yes, We do provide Shopify post- development maintenance and support services </p>
                 </div>
 
